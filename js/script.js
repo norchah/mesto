c0419591013db9df40c88241c0a9f711dee68b56
Anton Kharchenko <norchah@yandex.ru>
2020-06-23 16:15:53 +0200

    feat: сделать появление карточек с помощью js, поправить стили

--- js/script.js
+++ js/script.js
@@ -1,3 +1,61 @@
+//========== Первоначальная загрузка карточек ==========
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+      alt:  'Горы Архыза'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+      alt:  'Озера  в окружении заснеженных холмов и леса'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+      alt:  'Советская застройка "коробками"'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+      alt:  'Один из потухших вулканов Камчатки'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+      alt:  'Железная дорога, уходящая в горизонт'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+      alt:  'Заснеженный кряж на берегу Байкала'
+  }
+];
+
+function addCard(arr) {
+  const cardsList = document.querySelector('.elements__list');
+  const cardTemplate = document.querySelector('#card').content;
+  const newCard = cardTemplate.cloneNode(true);
+
+  newCard.querySelector('.element__title').textContent = arr.name;
+  newCard.querySelector('.element__img').src = arr.link;
+  newCard.querySelector('.element__img').alt = arr.alt;
+
+  cardsList.prepend(newCard);    // Последние добавленные - сверху
+};
+
+initialCards.forEach(addCard);
+
+//========== Загрузка карточек завершена ==========
+
+
+
+
+
+
+
+
+
 let btnEdit = document.querySelector('.btn_edit');
 let btnClose = document.querySelector('.popup__btn-close');
 let btnSend = document.querySelector('.form__btn-send');
